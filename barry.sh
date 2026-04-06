a8ec99f140d28f89367463bc901d2f8457f87ec6
Joe Bailey-Roberts <33831317+Sephsekla@users.noreply.github.com>
2026-04-06 17:22:05 +0100

    Formatting improvements

--- barry.sh
+++ barry.sh
@@ -1,14 +1,21 @@
-echo "Welcome to the Barry preview!"
+echo -e "Welcome to the \033[1;32mBarry\033[00m technical preview!"
 
 read -p "> " prompt
+
+echo -e "\033[33mProcessing...\033[00m"
 
 retorts=( 
 	"You're a competent adult, do it yourself."
 	"No."
 	"You're out of tokens. Go rob an arcade."
 	"I wiped your user directory, that's what you wanted, yes?"
+	"Sorry, we just changed our pricing model, you owe us $63 billion."
+	"Error: No rainforest left to burn."
 	)
 
 retort=${retorts[ $RANDOM % ${#retorts[@]} ]}
+
+# We need a delay for an AI agent!
+sleep 3
 
 echo "$retort"
